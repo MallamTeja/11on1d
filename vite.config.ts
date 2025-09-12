6c5c87debcce495f6b6418f27fce498ff8ad459e
tejamallam <tejamallam1233@gmail.com>
2025-09-12 19:50:20 +0530

    feat:

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    port: 5001,
     strictPort: true,
     proxy: {
       '/api': {
@@ -18,7 +22,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
 });
